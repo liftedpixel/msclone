fca70655163e6d49439cd009b305a59329dd2170
Jessica Pixel <liftedpixel@gmail.com>
2017-07-19 12:14:01 -0700

    day 1 progress

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+const blankLine = '   |   |   ';
+const guessLine = ' 1 |   |   ';
+const bombLine  = '   | B |   '
+
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+console.log('This is what a board with a guess and a bomb would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
