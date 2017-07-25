7d91a014ab293bf730d9dc2414ca5340d88d36dc
Jessica Pixel <liftedpixel@gmail.com>
2017-07-24 17:24:13 -0700

    add function to build game board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,4 @@
+/*
 const blankLine = '   |   |   ';
 const guessLine = ' 1 |   |   ';
 const bombLine  = '   | B |   '
@@ -11,3 +12,24 @@ console.log('This is what a board with a guess and a bomb would look like:');
 console.log(guessLine);
 console.log(bombLine);
 console.log(blankLine);
+*/
+
+const printBoard = (board) => {
+  console.log('Current Board:');
+  for (let q=0;q<=2;q++){
+    console.log(board[q].join(' | '));
+  }
+};
+
+const board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+printBoard(board);
+
+board[0][1] = 1;
+board[2][2] = 'B';
+
+printBoard(board);
